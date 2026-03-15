3046ccb6afd08a3ab481a746e7dc936041fde550
Beka <vsedlabrawla@gmail.com>
2026-03-15 14:17:47 +0500

    Base structure of commands.

--- handler/reader.go
+++ handler/reader.go
@@ -1,10 +1,24 @@
 package handler
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 
 func reader()  {
-	pass
+    file, err := os.Create("data.txt")
+    if err != nil {
+        panic(err)
+    }
+    defer file.Close()
+
+    data := []byte("hello")
+
+    _, err = file.Write(data)
+    if err != nil {
+        panic(err)
+    }
 	//1. Read the commands
 	//2. Give insturtion to the writer
 }
--- handler/writer.go
+++ handler/writer.go
@@ -1,8 +1,25 @@
 package handler
 
-import "fmt"
+import (
+    "fmt"
+    "os"
+)
 
 func writer()  {
+    file, err := os.Open("data.txt")
+    if err != nil {
+        panic(err)
+    }
+    defer file.Close()
+
+    buf := make([]byte, 10)
+
+    n, err := file.Read(buf)
+    if err != nil {
+        panic(err)
+    }
+
+    fmt.Println(string(buf[:n]))
 	//1. Answer the user
 	//2.Write the answers to the file
 }
